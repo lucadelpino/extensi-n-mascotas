19d25d7a22baf2392244a76d95694d26a22b9e22
lucadelpino <122765397+lucadelpino@users.noreply.github.com>
2024-09-09 16:39:28 -0300

    Update pajaro.java

--- mascotas/src/modelo/pajaro.java
+++ mascotas/src/modelo/pajaro.java
@@ -1,11 +1,14 @@
 package modelo;
 
 public class pajaro extends Mascota {
-
-public void lucirme() {
-		// TODO Auto-generated method stub
-		//super.lucirme();
-		
-		System.out.println("pio pio");
 	}
+	
+public class pajaro extends Mascota implements IVacunable{
+	
+	public void lucirme() {
+			// TODO Auto-generated method stub
+			//super.lucirme();
+			
+			System.out.println("pio pio");
+		}
 }
